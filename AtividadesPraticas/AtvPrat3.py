5b3b55bb8427adb46ff41eda8ee1664557bb0b46
Ribaldo237 <166262405+Ribaldo237@users.noreply.github.com>
2024-12-14 20:32:41 -0300

    Add files via upload

---
+++ AtividadesPraticas/AtvPrat3.py
@@ -0,0 +1,72 @@
+#Ativ1
+#contador = 0
+#while contador <= 10:
+#  print(contador)
+#  contador += 1
+
+#Ativ2
+#soma = 0
+#numero = 0
+#while numero <= 100:
+#  soma += numero
+#  numero += 1
+#print(f"A soma dos numero de 0 a 100 é: {soma}")
+
+#Ativ3
+#numero = int(input("Dijite o umero que voce deseja saber a tabuada: "))
+#contador = 1
+#while contador <= 10:
+#  resultado = numero * contador
+#  print(f"{numero} x {contador} = {resultado}")
+#  contador += 1
+
+#Ativ4
+#contador = 10
+#while contador >= 1:
+#  print(contador)
+#  contador -= 1
+#print("Feliz ano novo!")
+
+#Ativ5
+#numero_max = int(input("Dijite o numero maximo que deseje contar: "))
+#contador = 1
+#while contador <= (numero_max):
+#  print(contador)
+#  contador += 1
+#print("O numero atingiu o numero maximo.")
+
+#Ativ6
+#soma = 0
+#while True:
+#  numero = int(input("Digite os numero positivos qeu você quer somar (digite um numero negativo para sair):"))
+#  if numero < 0:
+#    break
+#  soma += numero
+#print(f"A soma dos numeros positivos é {soma}")
+
+#Ativ7
+#numero = int(input("Dijite o numero para mostra os resultados da taubada multiplos por 3: "))
+#contador = 1
+#while contador <= 10:
+#  resultado = numero * contador
+#  if resultado % 3 == 0:
+#    print(f"{numero} x {contador} = {resultado}")
+#  contador += 1
+
+#Ativ8
+#soma_notas = 0
+#Quantidade_notas = 0
+#while True:
+#  nota = float(input("Digite a nota que deseja somar (dijite -1 para cancelar e somar tudo): "))
+#  if nota == -1:
+#    break
+#  soma_notas += nota
+#  Quantidade_notas += 1
+#if Quantidade_notas > 0:
+#  media = soma_notas / Quantidade_notas
+# print(f"A quantidade de notas insiridas foram: {Quantidade_notas}")
+#  print(f"A media das notas foi {media:.2f}")
+#else:
+#  print("Nenhuma nota foi inserida.")
+
+#Ativ9
